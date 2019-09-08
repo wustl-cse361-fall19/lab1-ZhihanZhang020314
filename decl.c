6a6b4ffc949188c8e345708e533a0d1fedaf7f12
I-Ting Angelina Lee <angelee@gmail.com>
2019-09-08 15:34:20 -0500

    Initial commit

---
+++ decl.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+ {"bitXor", (funct_t) bitXor, (funct_t) test_bitXor, 2, "& ~", 14, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"evenBits", (funct_t) evenBits, (funct_t) test_evenBits, 0,
+    "! ~ & ^ | + << >>", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+{"byteSwap", (funct_t) byteSwap, (funct_t) test_byteSwap, 3,
+     "! ~ & ^ | + << >>", 25, 2,
+    {{TMin, TMax},{0,3},{0,3}}},
+ {"bitMask", (funct_t) bitMask, (funct_t) test_bitMask, 2,
+    "! ~ & ^ | + << >>", 16, 3,
+  {{0, 31},{0,31},{TMin,TMax}}},
+ {"logicalShift", (funct_t) logicalShift, (funct_t) test_logicalShift,
+   2, "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+// #include "rotateRight.c" /*3 = 13*/
+ {"bitCount", (funct_t) bitCount, (funct_t) test_bitCount, 1, "! ~ & ^ | + << >>", 40, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"fitsBits", (funct_t) fitsBits, (funct_t) test_fitsBits, 2,
+    "! ~ & ^ | + << >>", 15, 2,
+  {{TMin, TMax},{1,32},{TMin,TMax}}},
+ {"addOK", (funct_t) addOK, (funct_t) test_addOK, 2,
+    "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"satMul3", (funct_t) satMul3, (funct_t) test_satMul3, 1,
+    "~ & ^ | + << >>", 25, 3,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isPower2", (funct_t) isPower2, (funct_t) test_isPower2, 1, "! ~ & ^ | + << >>", 20, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+// #include "howManyBits.c" /*4 = 33*/
+ {"trueThreeFourths", (funct_t) trueThreeFourths, (funct_t) test_trueThreeFourths, 1,
+    "! ~ & ^ | + << >>", 20, 4,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+ {"float_i2f", (funct_t) float_i2f, (funct_t) test_float_i2f, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_twice", (funct_t) float_twice, (funct_t) test_float_twice, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
---
+++ tests.c
@@ -0,0 +1,174 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+int test_bitXor(int x, int y)
+{
+  return x^y;
+}
+int test_evenBits(void) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i+=2)
+    result |= 1<<i;
+  return result;
+}
+int test_byteSwap(int x, int n, int m)
+{
+    /* little endiamachine */
+    /* least significant byte stored first */
+    unsigned int nmask, mmask;
+    switch(n) {
+    case 0:
+      nmask = x & 0xFF;
+      x &= 0xFFFFFF00;
+      break;
+    case 1:
+      nmask = (x & 0xFF00) >> 8;
+      x &= 0xFFFF00FF;
+      break;
+    case 2:
+      nmask = (x & 0xFF0000) >> 16;
+      x &= 0xFF00FFFF;
+      break;
+    default:
+      nmask = ((unsigned int)(x & 0xFF000000)) >> 24;
+      x &= 0x00FFFFFF;
+      break;
+    }
+    switch(m) {
+    case 0:
+      mmask = x & 0xFF;
+      x &= 0xFFFFFF00;
+      break;
+    case 1:
+      mmask = (x & 0xFF00) >> 8;
+      x &= 0xFFFF00FF;
+      break;
+    case 2:
+      mmask = (x & 0xFF0000) >> 16;
+      x &= 0xFF00FFFF;
+      break;
+    default:
+      mmask = ((unsigned int)(x & 0xFF000000)) >> 24;
+      x &= 0x00FFFFFF;
+      break;
+    }
+    nmask <<= 8*m;
+    mmask <<= 8*n;
+    return x | nmask | mmask;
+}
+int test_bitMask(int highbit, int lowbit)
+{
+  int result = 0;
+  int i;
+  for (i = lowbit; i <= highbit; i++)
+    result |= 1 << i;
+  return result;
+}
+int test_logicalShift(int x, int n) {
+  unsigned u = (unsigned) x;
+  unsigned shifted = u >> n;
+  return (int) shifted;
+}
+// #include "rotateRight.c" /*3 = 13*/
+int test_bitCount(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i++)
+    result += (x >> i) & 0x1;
+  return result;
+}
+int test_fitsBits(int x, int n)
+{
+  int TMin_n = -(1 << (n-1));
+  int TMax_n = (1 << (n-1)) - 1;
+  return x >= TMin_n && x <= TMax_n;
+}
+int test_addOK(int x, int y)
+{
+    long long lsum = (long long) x + y;
+    return lsum == (int) lsum;
+}
+int test_satMul3(int x)
+{
+  if ((x+x+x)/3 != x)
+    return x < 0 ? 0x80000000 : 0x7FFFFFFF;
+  else
+    return 3*x;
+}
+int test_isPower2(int x) {
+  int i;
+  for (i = 0; i < 31; i++) {
+    if (x == 1<<i)
+      return 1;
+  }
+  return 0;
+}
+// #include "howManyBits.c" /*4 = 33*/
+int test_trueThreeFourths(int x)
+{
+  return (int) (((long long int) x * 3)/4);
+}
+unsigned test_float_i2f(int x) {
+  float f = (float) x;
+  return f2u(f);
+}
+unsigned test_float_twice(unsigned uf) {
+  float f = u2f(uf);
+  float tf = 2*f;
+  if (isnan(f))
+    return uf;
+  else
+    return f2u(tf);
+}
