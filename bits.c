80e9e75f4bbbd97b2e83ae26621d6cc394fd2038
ZhihanZhang020314 <54601270+ZhihanZhang020314@users.noreply.github.com>
2019-09-08 22:56:23 -0500

    Update bits.c

--- bits.c
+++ bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * <Zhihan Zhang 478594>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
